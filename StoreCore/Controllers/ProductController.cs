7e66699c4fde4ecf2d0853620188adc1c95028c2
zwb800 <zwb800@gmail.com>
2018-10-21 18:09:09 +0800

    微信支付

--- StoreCore/Controllers/ProductController.cs
+++ StoreCore/Controllers/ProductController.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Senparc.Weixin.MP;
+using Senparc.Weixin.MP.TenPayLibV3;
 using StoreCore.Models;
 
 namespace StoreCore.Controllers
@@ -10,9 +13,15 @@ namespace StoreCore.Controllers
     [Route("[controller]")]
     public class ProductController : Controller
     {
+        public ProductController(IHttpContextAccessor accessor)
+        {
+            Accessor = accessor;
+        }
+
         storeContext storeContext = new storeContext();
 
         public object ID { get; private set; }
+        public IHttpContextAccessor Accessor { get; set; }
 
         public IEnumerable<ProductListViewModel> Get(int page = 1)
         {
@@ -40,17 +49,7 @@ namespace StoreCore.Controllers
         public string ShippingOrder([FromBody] OrderData orderData ) {
             var address = orderData.address;
             address.UserId = orderData.UserID;
-            if (!storeContext.Address.Any(p => 
-            p.UserId == address.UserId&&
-            p.UserName == address.UserName && 
-            p.TelNumber == address.TelNumber &&
-             p.DetailInfo == address.DetailInfo && 
-             p.CountyName == address.CountyName && 
-             p.CityName == address.CityName && 
-             p.ProvinceName == address.ProvinceName)){
-                storeContext.Address.Add(address);
-                storeContext.SaveChanges();
-            }
+            SaveAddress(address);
 
             var order = new Order()
             {
@@ -71,6 +70,108 @@ namespace StoreCore.Controllers
             }
             storeContext.SaveChanges();
             return "success";
+        }
+
+        [Route("[action]")]
+        public Unified WxPayOrder([FromBody] OrderData orderData)
+        {
+            var address = orderData.address;
+            address.UserId = orderData.UserID;
+            SaveAddress(address);
+
+            var order = new Order()
+            {
+                CreateTime = DateTime.Now,
+                UserId = orderData.UserID,
+                AddressId = address.Id,
+            };
+            storeContext.Order.Add(order);
+            storeContext.SaveChanges();
+
+            decimal sumPrice = 0;
+
+            var skus = storeContext.Sku.Where(p => orderData.skus.Select(p1 => p1.ID).Contains(p.Id)).ToArray();
+
+            foreach (var sku in orderData.skus)
+            {
+                sumPrice += (skus.Single(p => p.Id == sku.ID).Price * sku.Count);
+
+                storeContext.Ordersku.Add(new Ordersku()
+                {
+                    Skuid = sku.ID,
+                    Count = sku.Count,
+                    OrderId = order.Id,
+
+                });
+
+
+            }
+            storeContext.SaveChanges();
+
+    
+            return WxPay("",(int)(sumPrice*100),"");
+        }
+
+        private void SaveAddress(Address address) {
+            if (!storeContext.Address.Any(p =>
+            p.UserId == address.UserId &&
+            p.UserName == address.UserName &&
+            p.TelNumber == address.TelNumber &&
+             p.DetailInfo == address.DetailInfo &&
+             p.CountyName == address.CountyName &&
+             p.CityName == address.CityName &&
+             p.ProvinceName == address.ProvinceName))
+            {
+                storeContext.Address.Add(address);
+                storeContext.SaveChanges();
+            }
+        }
+
+
+        private TenPayV3Info TenPayV3Info = new TenPayV3Info("wxd386c7dc2ba5ab39", "5068dce882d9eb962bdeb1fd0ad32d33", "1499945642", "wLClWzYk5v6hTCLvmXHUmHVQRNwzAMdD", "");
+
+        [Route("[action]")]
+        public Unified WxPay(string body,int price,string openId) {
+            var timeStamp = TenPayV3Util.GetTimestamp();
+            var nonceStr = TenPayV3Util.GetNoncestr();
+
+            //生成订单10位序列号，此处用时间和随机数生成，商户根据自己调整，保证唯一
+            var sp_billno = string.Format("{0}{1}{2}", TenPayV3Info.MchId/*10位*/, DateTime.Now.ToString("yyyyMMddHHmmss"),
+                TenPayV3Util.BuildRandomStr(6));
+            var xmlDataInfo = new TenPayV3UnifiedorderRequestData(
+                TenPayV3Info.AppId, 
+                TenPayV3Info.MchId, 
+                body, 
+                sp_billno, 
+                price, 
+                Accessor.HttpContext.UserHostAddress().ToString(), 
+                TenPayV3Info.TenPayV3Notify, 
+                TenPayV3Type.JSAPI, 
+                openId, 
+                TenPayV3Info.Key, 
+                nonceStr);
+
+            var result = TenPayV3.Unifiedorder(xmlDataInfo);//调用统一订单接口
+                                                            //JsSdkUiPackage jsPackage = new JsSdkUiPackage(TenPayV3Info.AppId, timeStamp, nonceStr,);
+            var package = string.Format("prepay_id={0}", result.prepay_id);
+
+            return new Unified {
+                timeStamp = timeStamp,
+                nonceStr = nonceStr,
+                package = package,
+                paySign = TenPayV3.GetJsPaySign(TenPayV3Info.AppId, timeStamp, nonceStr, package, TenPayV3Info.Key)
+            };
+        }
+
+        
+
+        public class Unified {
+            public string timeStamp { get; set; }
+            public string nonceStr { get; set; }
+
+            public string package { get; set; }
+
+            public string paySign { get; set; }
         }
 
         public class OrderData {
